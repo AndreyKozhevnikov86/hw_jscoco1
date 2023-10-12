2cdd62b695fed76ef44b41772698e4168e8b5550
AndreyKozhevnikov86 <orion128@mail.ru>
2023-10-12 15:23:00 +0300

    extra test

--- src/test/java/ru/netology/qa/StatisticsServiceTest.java
+++ src/test/java/ru/netology/qa/StatisticsServiceTest.java
@@ -4,6 +4,7 @@ package ru.netology.qa;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
     @Test
     void findMax() {
@@ -16,4 +17,18 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxFromMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
+        long expected = 30;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
+
